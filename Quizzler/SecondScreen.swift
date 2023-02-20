f16726f2a701b68923196e680eceeeb380143dff
akar-jaza <64991287+akar-jaza@users.noreply.github.com>
2023-02-20 23:18:41 +0300

    new Class added

---
+++ Quizzler/SecondScreen.swift
@@ -0,0 +1,29 @@
+//
+//  SecondScreen.swift
+//  Quizzler
+//
+//  Created by Akar jaza on 2/20/23.
+//
+
+import UIKit
+
+class SecondScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -11,13 +11,29 @@ class firstScreen: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.systemMint
+        
+        view.backgroundColor = .systemBackground
+        self.title = "First Screen"
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor:UIColor.white]
         
         let trueBtn = UIButton()
+        setupButton()
         
         func setupButton() {
             view.addSubview(trueBtn)
             trueBtn.configuration = .filled()
+            trueBtn.configuration?.title = "True"
+            trueBtn.configuration?.baseBackgroundColor = .systemRed
+            
+            trueBtn.translatesAutoresizingMaskIntoConstraints = false
+
+            NSLayoutConstraint.activate([
+                trueBtn.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                trueBtn.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+                trueBtn.widthAnchor.constraint(equalToConstant: 200),
+                trueBtn.heightAnchor.constraint(equalToConstant: 50)
+            ])
+            
         }
         
     }
